96d6ea14a5acf2702f3c5cf6430d0be3d7cfe75a
sergiogt93 <sergio.ber0556@gmail.com>
2022-02-11 11:00:12 +0100

    Logical Personal Information profile

--- src/components/PersonalInformation/PersonalInformation.js
+++ src/components/PersonalInformation/PersonalInformation.js
@@ -1,16 +1,20 @@
 import './personalInformation.scss';
 
-function PersonalInformation({ name = '', email = '', city = '', country = ''}) {
+
+function PersonalInformation({ name, email, city, country, description}) {
+    const avatarPerfil = "https://avatars.dicebear.com/api/micah/" + name + ".svg";
+
     return (
         <section className='personalInfo'>
             <article>
-                <img alt='avatarPerfil'/>
+                <img src={avatarPerfil} alt='avatarPerfil' className='profileAvatar'/>
             </article>
             <article>
                 <h2>{name !== null && name}</h2>
                 <p>{email !== null && email}</p>
                 <p>{city !== null && city + ","} {country !== null && country}</p>
             </article>
+            <p>{description !== null && description}</p>
         </section>
     );
 }
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,21 +1,25 @@
 import './profile.scss';
 import PersonalInformation from '../../components/PersonalInformation';
 
-function Profile(pops) {
+function Profile() {
 
     const userData = {
         name: localStorage.getItem("name"),
         email: localStorage.getItem("email"),
         city: localStorage.getItem("city"),
-        country: localStorage.getItem("country")
+        country: localStorage.getItem("country"),
+        description: localStorage.getItem("description")
     };
 
     return (
         <main>
-            <PersonalInformation name={userData.name}
+            <PersonalInformation
+                name={userData.name}
                 email={userData.email}
                 city={userData.city}
-                country={userData.country} />
+                country={userData.country}
+                description={userData.description}
+            />
         </main>
     );
 }
