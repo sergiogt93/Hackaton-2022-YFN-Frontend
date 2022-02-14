df896db892a10cebcc554691c0a120031ba0d9cb
sergiogt93 <sergio.ber0556@gmail.com>
2022-02-14 11:06:38 +0100

    BD forget implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import FormPersonalUser from './components/User/PersonalInforUser';
 import '../node_modules/boxicons/css/boxicons.min.css';
 import './scss/app.scss';
 
+localStorage.setItem("userData", JSON.stringify({
+  name: "nombre",
+  email: "correo",
+  description: "descripcion",
+  city: "ciudad",
+  country: "pais",
+}))
+localStorage.setItem("workUser", JSON.stringify({
+  experienceYear: 0,
+  sector: "sector",
+}))
+localStorage.setItem("skillsUser", JSON.stringify([
+]))
+
 function App() {
   const session = localStorage.getItem('userData');
 
